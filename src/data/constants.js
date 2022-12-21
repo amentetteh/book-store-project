56614691e86780fe86f5d00dd86a339dfbe23d2f
Amen TETTEH <tkamen@outlook.com>
2022-12-21 00:38:42 +0000

    Add data folder for datas

---
+++ src/data/constants.js
@@ -0,0 +1,17 @@
+export const menuLinks = [
+  {
+    id: 1,
+    path: '/',
+    text: 'Books',
+    element: '<Books />',
+  },
+  {
+    id: 2,
+    path: '/categories',
+    text: 'Categories',
+    element: '<Categories />',
+  },
+];
+export const ADD_BOOK = 'ADD_BOOK';
+export const REMOVE_BOOK = 'REMOVE_BOOK';
+export const CHECK_STATUS = 'CHECK_STATUS';
