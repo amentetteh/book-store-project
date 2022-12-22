ec5ff11be685dfe2e9cbf55045794cae4beea20e
Amen TETTEH <tkamen@outlook.com>
2022-12-22 14:05:58 +0000

    Update Categories.js file to add redux

--- src/components/category/Categories.js
+++ src/components/category/Categories.js
@@ -1,5 +1,21 @@
-function Categories() {
-  return <button type="button">Check status</button>;
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { checkStatus } from '../../redux/categories/categories';
+
+function Categories(props) {
+  const { categories } = props;
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <button type="button" onClick={() => dispatch(checkStatus())}>
+        Check status
+      </button>
+      <div>{categories}</div>
+    </div>
+  );
 }
+Categories.propTypes = {
+  categories: PropTypes.string.isRequired,
+};
 
 export default Categories;
