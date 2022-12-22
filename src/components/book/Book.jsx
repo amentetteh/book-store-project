9908f2451eedcc5c686d4af42a488605d3bcfdc4
Amen TETTEH <tkamen@outlook.com>
2022-12-22 14:04:32 +0000

    Update Book.jsx file to add redux

--- src/components/book/Book.jsx
+++ src/components/book/Book.jsx
@@ -1,16 +1,21 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../../redux/books/books';
 
 const Book = (props) => {
-  const { title, author } = props;
+  const dispatch = useDispatch();
+  const { id, title, author } = props;
   return (
     <li>
+      {id}
       {title}
       {author}
-      <button type="button">Delete</button>
+      <button type="button" onClick={() => dispatch(removeBook({id}))}>Delete</button>
     </li>
   );
 };
 Book.propTypes = {
+  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
 };
