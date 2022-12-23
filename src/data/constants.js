9464d6b8f9b3bf31cc904e843e3f0d5862305c19
Amen TETTEH <tkamen@outlook.com>
2022-12-23 21:00:43 +0000

    update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -2,6 +2,6 @@ export const ADD_BOOK = 'book-store-project/books/ADD_BOOK';
 export const REMOVE_BOOK = 'book-store-project/books/REMOVE_BOOK';
 export const CHECK_STATUS = 'book-store-project/categories/CHECK_STATUS';
 
-const APP_ID = 'Ee3DxhwW3t70Nyl1c1Gj';
+const APP_ID = 'MTi8Ny9NMmhVSAhAEL15';
 const BASE_URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${APP_ID}`;
 export const BOOK_URL = `${BASE_URL}/books`;
